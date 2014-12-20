587cb7bcf819dc413fc8e4da45f6ece6c2f09320
niksxs <niksxs@googlemail.com>
2014-12-20 19:14:02 +0100

    menü erstellt

--- app/src/main/java/com/example/niklas/projekt/NewPlanActivity.java
+++ app/src/main/java/com/example/niklas/projekt/NewPlanActivity.java
@@ -4,28 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class NewPlanActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_plan);
     }
 
-    public void buttonOnClick(View v) {
-        // do something when the button is clicked
-        Button button = (Button) v;
-        ((Button) v).setText("clicked");
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_new_plan, menu);
         return true;
     }
 
